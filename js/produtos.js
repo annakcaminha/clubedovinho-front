80d2b66fb928d394abab6bef2347e024ef58f4b6
unknown <annaaraujo0631@digitalcollege.internal>
2025-03-13 21:56:52 -0300

    Instalação de bootstrap e uso de API teste

--- js/produtos.js
+++ js/produtos.js
@@ -1,49 +1,46 @@
 const TABLE = document.getElementById('table-products');
 
 
-let itens = [ 
-    {   
-        id: 1,
-        nome: 'Quinta do Morgado',
-        categoria: 'Vinho Tinto Suave',
-        imagem: 'https://images.rappi.com.br/products/313306468675_jwsfzseujjas_124100922271_ixnzygevdpsn_227353_1.png',
-        quantidade: 20,
-        valor: 10.90,
-
-    },
-    {   
-        id: 2,
-        nome: 'Dom Bento',
-        categoria: 'Vinho Tinto Suave',
-        imagem: 'https://cdn.awsli.com.br/1958/1958204/produto/203245775/screenshot_20230223_190651_instagram-pamtvr.jpg',
-        quantidade: 10,
-        valor: 2.90,
-
-    },
-]
-
-itens.forEach(addLinha);
-
+fetch('http://localhost:3000/products')
+    .then(res => res.json())
+    .then(dados => {
+        dados.forEach(addLinha);
+    });
 
 function addLinha(cada_item) {
     TABLE.innerHTML += `
     
      <tr>
         <td>${cada_item.id}</td>
-        <td>${cada_item.nome}</td>
-        <td>${cada_item.categoria}</td>
-        <td><img onclick="abrirModal('${cada_item.nome}', '${cada_item.imagem}')" data-bs-toggle="modal" data-bs-target="#exampleModal" src="${cada_item.imagem}" width="100px"></td>
-        <td>${cada_item.quantidade}</td>
-        <td>${cada_item.valor}</td>
+        <td>${cada_item.name}</td>
+        <td>${cada_item.category}</td>
+        <td><img onclick="abrirModal('${cada_item.name}', '${cada_item.image}')" data-bs-toggle="modal" data-bs-target="#exampleModal" src="${cada_item.imagem}" width="100px"></td>
+        <td>${cada_item.quantity}</td>
+        <td>${cada_item.price}</td>
         <td>
             <a href="#" class="btn btn-outline-warning btn-sm">Editar</a>
-            <a href="#" class="btn btn-outline-danger btn-sm">Excluir</a>
+            <a href="#" onclick="excluir(${cada_item.id})" class="btn btn-outline-danger btn-sm">Excluir</a>
         </td>
      </tr>
 
 
     `
 }
+
+function excluir(id) {
+   if (false === confirm ('Confirma ou sem firma?')) {
+        return;
+   }
+   
+    fetch(`http://localhost:3000/products/${id}`, {
+        method: 'DELETE'
+    });
+
+    alert("Produto excluído");
+
+    location.href = "";
+}
+
 
 function abrirModal (nome, imagem) {
     document.getElementById('exampleModalLabel').innerHTML = nome;
